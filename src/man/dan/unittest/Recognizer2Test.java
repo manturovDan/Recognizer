e4fcf0ced4ee343597a4704e1459a2b1e0d37f20
manturovDan <manturov.dan@mail.ru>
2020-03-03 01:16:02 +0300

    before tests

---
+++ src/man/dan/unittest/Recognizer2Test.java
@@ -0,0 +1,15 @@
+package man.dan.unittest;
+
+import man.dan.smc.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Recognizer2Test {
+    @Test
+    public void testEquals() {
+        Assert.assertEquals(4, 2 + 2);
+        Assert.assertTrue(4 == 2 + 2);
+    }
+}
